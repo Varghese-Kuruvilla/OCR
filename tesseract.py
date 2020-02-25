61f23dbfcf09ad0fceb23201d25346959271a540
Vpk <vpk010896@gmail.com>
2020-02-25 19:26:26 +0530

    ocr

--- tesseract.py
+++ tesseract.py
@@ -40,6 +40,12 @@ class ocrutils:
         #Image for drawing contours
         cnt_img = np.copy(self.img)
 
+        #Local variables
+        box_coord_ls = []
+        merged_coord_ls = []
+        threshold = 10
+        flag = False
+
         display("Input image",self.img)
         self.gray_img = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
         display("Grayscale image",self.gray_img)
@@ -51,7 +57,7 @@ class ocrutils:
         #Finding Contours on the image and extracting the largest one
         contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
         large_contour = sorted(contours, key=cv2.contourArea, reverse=True)[:1] #Extract only the table
-        print("len(contours)",len(contours))
+        # print("len(contours)",len(contours))
 
         table_mask = np.zeros((self.img.shape[0],self.img.shape[1]),dtype = np.uint8)
         table_mask = cv2.drawContours(table_mask,large_contour,0,(255,255,255),-1)
@@ -63,11 +69,53 @@ class ocrutils:
 
         #Now we find each contour within the table and give it as input to OCR
         contours, hierarchy = cv2.findContours(table_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
-        cell_contours = sorted(contours,key=cv2.contourArea,reverse=True)
+        cell_contours = sorted(contours,key=cv2.contourArea,reverse=True)[:30]
 
         for cnt in cell_contours:
-            cv2.drawContours(cnt_img, [cnt], 0, (0,255,0), 3)
-        display("Contour Image",cnt_img)
+            x,y,w,h = cv2.boundingRect(cnt)
+
+            #Merge neighbouring contours
+            box_coord = np.array([x,y,w,h]).reshape(4,-1)
+            print("box_coord:",box_coord)
+            
+
+            #Compare 
+            for i in range(0,len(box_coord_ls)):
+            # for box_coord_ls[i] in box_coord_ls:
+                if(abs((box_coord_ls[i][0,0] + box_coord_ls[i][2,0]) - box_coord[0,0]) < threshold and abs(box_coord_ls[i][1,0] - box_coord[1,0]) < threshold):
+                    flag = True
+                    print("Inside true condition")
+                    xmin = box_coord_ls[i][0,0]
+                    ymin = box_coord_ls[i][1,0]
+                    xmax = box_coord_ls[i][0,0] + box_coord_ls[i][2,0] + box_coord[2,0]
+                    ymax = box_coord_ls[i][1,0] + box_coord_ls[i][3,0]
+
+                    element = np.array([xmin,ymin,xmax-box_coord_ls[i][0,0],ymax-box_coord_ls[i][1,0]]).reshape(4,-1)
+                    box_coord_ls[i] = element
+                    merged_coord = np.array([xmin,ymin,xmax,ymax]).reshape(4,-1)
+                    merged_coord_ls.append(merged_coord)
+
+            if(flag == False): 
+                box_coord_ls.append(box_coord)
+                print("box_coord:",box_coord)
+            flag = False
+        
+        #Draw rectangles
+        for coord in box_coord_ls:
+            cv2.rectangle(cnt_img,(coord[0,0],coord[1,0]),(coord[0,0] + coord[2,0],coord[1,0] + coord[3,0]),(0,0,255),5)
+            display("Contour Image",cnt_img)
+        
+        
+            #Filtering out false contours based on length of the rectangle
+            # if(w >= 300 and w <= 400): #Assuming a controlled environment
+            #if((w > 900 and w < 1050) and (h > 90 and h<130)):
+                #For debug
+                # cv2.drawContours(cnt_img, [cnt], 0, (0,255,0), 3)
+                # cv2.rectangle(cnt_img,(x,y),(x+w,y+h),(0,255,0))
+                # Pass this to OCR
+            #    img_crop = self.img[y:y+h,x:x+w]
+            #    display("img_crop",img_crop)
+        #display("Contour Image",cnt_img)
         # breakpoint()
 
 
